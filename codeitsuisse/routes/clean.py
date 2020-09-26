4ee2b7ce68fbdf59a96c817aa047571540f3b302
PeePeeDante <charleston@xujiafus-MacBook-Pro.local>
2020-09-26 19:10:50 +0800

    submit clean-3

--- codeitsuisse/routes/clean.py
+++ codeitsuisse/routes/clean.py
@@ -33,8 +33,13 @@ def stepsRequired(floor):
             elif (floor[i] == 0) and (floor[i+1] == 0):
                 return total_steps
         total_steps += 2*floor[i]+1
-        floor[i+1] = abs(floor[i+1]-floor[i]-1)
-        
+        if (floor[i+1]-floor[i]-1>=0):
+            floor[i+1] = floor[i+1]-floor[i]-1
+        else:
+            if ((floor[i+1]-floor[i]-1)%2):
+                floor[i+1]=1
+            else:
+                floor[i+1]=0
         
     remain = floor[len(floor)-1]
     if (remain%2):
@@ -42,12 +47,3 @@ def stepsRequired(floor):
     else:
         total_steps += 2*remain
     return total_steps
-
-
-        
-
-
-
-
-
-
